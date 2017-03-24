1c8580257117c301b95bffd42613474c88342b28
Jeosadache <josa@luizalabs.com>
2017-03-24 10:49:37 -0300

    product handler lib

--- server.js
+++ server.js
@@ -2,66 +2,8 @@
 
 const
   Hapi = require('hapi'),
-  request = require('request'),
-  server = new Hapi.Server();
-
-/**
- * Retrieve product prices
- * 
- * @return {Array} prices
- */
-let getPrices = () => {
-
-  var options = {
-    uri: 'http://www.mocky.io/v2/58d42dd71000006c0ed7a6f4',
-    method: 'GET',
-    json: true
-  }
-
-  return new Promise((resolve, reject) => {
-    request(options, (err, response, body) => {
-      if(err){
-        return reject(err);
-      }
-
-      resolve(body);
-    });
-  });
-};
-
-/**
- * Retrive products
- * 
- * @return {Array} products
- */
-let getProducts = async () => {
-  return [{
-    id: 1933885,
-    name: "Smart TV LED 70\" Samsung 4K/Ultra HD 70KU6000"
-  }, {
-    id: 2165147,
-    name: "Smartphone Motorola Moto G 4ª Geração Play DTV"
-  }];
-};
-
-/**
- * 
- * @param {hapi.Request} req 
- * @param {hapi.Reply} reply 
- */
-let productHandle = async (req, reply) => {
-
-  let [products, prices] = await Promise.all(
-    [getProducts(), getPrices()]
-  ).catch(err => reply(err));
-
-  let data = products.map(p => {
-    let price = prices.find(pr => p.id == pr.id);
-    return Object.assign(p, price);
-  });
-
-  reply(data);
-};
+  server = new Hapi.Server(),
+  productHandler = require("./lib/productHandler.js");
 
 server.connection({
   host: '0.0.0.0',
@@ -71,7 +13,7 @@ server.connection({
 server.route({
   method: 'GET',
   path: '/products',
-  handler: productHandle
+  handler: productHandler
 });
 
-module.exports = server
+module.exports = server;
--- test/productTest.js
+++ test/productTest.js
@@ -38,8 +38,9 @@ describe("Products", () => {
 
   it("GET /product response should have product: \"Smart TV LED 70\"\"", (done) => {
 
+    let name = "Smart TV LED 70\" Samsung 4K/Ultra HD 70KU6000";
+
     server.inject(reqOpts, (res) => {
-      let name = "Smart TV LED 70\" Samsung 4K/Ultra HD 70KU6000";
       let found = res.result.find((p) => p.name == name);
 
       should(found).be.ok();
